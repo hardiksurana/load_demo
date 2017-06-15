f93790ac7c994113b33849719dcb2f18a54456e8
hardiksurana <hardiksurana01@gmail.com>
2017-06-15 17:09:23 +0530

    removed event listener 5

--- JS/Hotspots.js
+++ JS/Hotspots.js
@@ -320,10 +320,9 @@ var renderHotspot = function(hotspot){
 
 
 var renderAnimationOrLoader = function(sceneName) {
-    // console.log(e);
     if(ImgSet.has(sceneName)) {
-        fadeAnimation(1, 0, 3000);
-        fadeAnimation(0, 1, 3000);
+        fadeAnimation(1, 0, 1000);
+        fadeAnimation(0, 1, 1000);
     } else {
         document.querySelector('a-sky').setAttribute('color', '');
         document.querySelector('#loader_entity').setAttribute('visible', false);
