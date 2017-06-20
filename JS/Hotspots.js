187e9feca578b450c389f60531324ef6510d99a3
hardiksurana <hardiksurana01@gmail.com>
2017-06-20 17:17:25 +0530

    [WIP] testing loader issue in some scenes 7

--- JS/Hotspots.js
+++ JS/Hotspots.js
@@ -363,8 +363,9 @@ var renderAnimations = function (tempScene) {
                         console.log('sky color is removed');
                         document.querySelector('a-sky').setAttribute('color', '');
                         console.log('loader visibility is false');
-                        document.querySelector('#loader_entity').setAttribute('visible', "false");
+                        // document.querySelector('#loader_entity').setAttribute('visible', false);
                         // document.querySelector('#loader_entity').className = 'loader';
+                        document.querySelector('#loader_entity').visible = false;
                         console.log('shown loader is removed. end of function.');
                         LoaderShown = false;
                     }, 2000);
@@ -455,9 +456,10 @@ var getReticlePosition = function(){
 var setLoader = function() {
     var position = getReticlePosition();
     document.querySelector('#loader_entity').setAttribute('position', `${position.x} ${position.y} ${position.z}`);
-    document.querySelector('#loader_entity').setAttribute('visible', "true");
+    // document.querySelector('#loader_entity').setAttribute('visible', true);
     console.log('loader_entity is set. In setLoader function.');
     // document.querySelector('#loader_entity').classList.remove('loader');
+    document.querySelector('#loader_entity').visible = true;
     document.querySelector('a-sky').setAttribute('color', '#293f59');
     console.log('sky color is added.');
     LoaderShown = true;
